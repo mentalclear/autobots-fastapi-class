365f43695b3599d821c5f36672db834710a70f69
Dmitrii Kilishek <mentalclear@gmai.com>
2021-08-07 15:51:02 -0400

    some minor changes to test and stuff

--- challenges/utils_challenge/tests/test_string_utils.py
+++ challenges/utils_challenge/tests/test_string_utils.py
@@ -2,11 +2,17 @@ from utils.string_utils import *
 
 def test_string_repeater_positive():
     character = "a"
-    cout = 4
-    assert string_repeater(character, cout) == "aaaa"
+    count = 4
+    assert string_repeater(character, count) == "aaaa"
 
+# Negatvie test for length
 def test_string_repeater_negative():
     character = "a"
-    cout = 5
-    assert string_repeater(character, cout) != "aaaa"
+    count = 5
+    assert string_repeater(character, count) != "aaaa"
 
+# Negative negative test for characters
+def test_strin_repeater_negative_character():
+    character = "b"
+    str_length = 5
+    assert string_repeater(character, str_length) != "aaaaa"
