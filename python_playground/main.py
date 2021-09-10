6e403d03b5a04f664d3dc9e0c9ca13fed8cc2b4f
Dmitrii Kilishek <mentalclear@gmail.com>
2021-09-09 22:27:08 -0400

    Updating with session 3 progress

--- python_playground/main.py
+++ python_playground/main.py
@@ -1,3 +1,62 @@
-print("Hello")
+from fastapi import FastAPI
 
-print("Hello Again!")
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/api")
+async def api():
+    return {"message": "Hello from the API"}
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     # Now I can do anything with item_id realated to int
+#     # remove type thing and doing math will result in server error
+#     return {"item_id": item_id * 2}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]    
+
+
+@app.get("/hello/")    
+async def greeting(first_name: str, age: int):
+    return f"Hello, {first_name}. It looks like you are {age} years old!"
+
+
+from typing import Optional
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
