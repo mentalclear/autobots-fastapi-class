fcac1813e9e22e098acee136225747c139995a1a
Dmitrii Kilishek <mentalclear@gmail.com>
2021-09-16 08:10:03 -0400

    played with async and await

---
+++ python_playground_wasync/async_say.py
@@ -0,0 +1,16 @@
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    print(f"Started at {time.strftime('%X')}")
+    await say_after(1, 'Hello')
+    print("How about this?!")
+    await say_after(2, 'World')
+
+    print(f"Finished at {time.strftime('%X')}")
+
+asyncio.run(main())
