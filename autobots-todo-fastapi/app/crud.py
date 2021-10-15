7a6b396702724a1673672915104702f41b5c0669
Dmitrii Kilishek <mentalclear@gmail.com>
2021-10-14 23:27:35 -0400

    Sesstion 5 completed

--- autobots-todo-fastapi/app/crud.py
+++ autobots-todo-fastapi/app/crud.py
@@ -1,11 +1,22 @@
 from sqlalchemy.orm import Session
 
+# from sqlalchemy.exc import PendingRollbackError
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException, status
+
 from app import models, schemas
 
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(username=user.username, password=user.password)
     db.add(db_user)
-    db.commit()
+    try:
+        db.commit()
+    except IntegrityError as err:
+
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="Username already registered"
+        )
+
     db.refresh(db_user)
     return db_user
--- autobots-todo-fastapi/tests/test_app.py
+++ autobots-todo-fastapi/tests/test_app.py
@@ -1,16 +1,40 @@
+# from fastapi.testclient import TestClient
+
+# # import requests
+# from app.main import app
+
+# client = TestClient(app)
+
+
+# def test_root_route():
+#     # same with requests:
+#     # response = requests.get("http://localhost:8000")
+#     # assert response.ok
+
+#     response = client.get("/")
+#     assert response.ok
+#     assert response.json()["msg"] == "Hello World"
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from app import models
 
-# import requests
-from app.main import app
+def test_create_user(client: TestClient):
+    response  = client.post(
+        "/create-user", json={"username":"Test1", "password":"Test1"}
+    )
+    data = response.json()
+    assert response.status_code == 200
+    assert data['id'] is not None
+    
 
-client = TestClient(app)
+def test_create_user_raises_username_taken(session: Session, client: TestClient):
+    user_1 = models.User(username="Test1", password="Test1")
+    session.add(user_1)
+    session.commit()
 
-
-def test_root_route():
-    # same with requests:
-    # response = requests.get("http://localhost:8000")
-    # assert response.ok
-
-    response = client.get("/")
-    assert response.ok
-    assert response.json()["msg"] == "Hello World"
+    response = client.post(
+        "/create-user",  json={"username":"Test1", "password":"Test1"}
+    )    
+    data = response.json()
+    assert response.status_code == 409
+    assert data['detail'] == "Username already registered"
